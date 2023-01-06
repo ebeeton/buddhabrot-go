f197b850286d1ac3c17d6e87e90ce29a4ad0e6a4
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-01-06 17:40:12 -0500

    Add channel and rgbPlot parameters.

---
+++ parameters/channel.go
@@ -0,0 +1,8 @@
+// Package parameters defines parameters used to plot Buddhabrot images.
+package parameters
+
+type channel struct {
+	sampleSize,
+	maxSampleIterations,
+	maxIterations uint64
+}
---
+++ parameters/rgbplot.go
@@ -0,0 +1,8 @@
+// Package parameters defines parameters used to plot Buddhabrot images.
+package parameters
+
+type rgbPlot struct {
+	red,
+	green,
+	blue channel
+}
