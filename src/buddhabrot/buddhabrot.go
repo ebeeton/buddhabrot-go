004f9abbd1e81095cbb743fb90ca24a8a30eb489
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-01-07 16:36:12 -0500

    Add support functions and tests.

--- src/buddhabrot/buddhabrot.go
+++ src/buddhabrot/buddhabrot.go
@@ -1,7 +1,11 @@
 // Package Buddhabrot plots images.
 package buddhabrot
 
-import "github.com/ebeeton/buddhalbrot-go/parameters"
+import (
+	"math"
+
+	"github.com/ebeeton/buddhalbrot-go/parameters"
+)
 
 const (
 	Channels = 3
@@ -13,4 +17,21 @@ func Plot(plot parameters.RgbPlot) [][]uint32 {
 		counter[i] = make([]uint32, plot.Height*plot.Width)
 	}
 	return counter
+}
+
+func isInMandelbrotSet(c complex128, maxIterations int) (bool, int) {
+	const bailout float64 = 2
+	z := c
+	for i := 0; i < maxIterations; i++ {
+		if math.Abs(real(z)) > bailout || math.Abs(imag(z)) > bailout {
+			return false, i
+		}
+		z = z*z + c
+	}
+
+	return true, maxIterations
+}
+
+func linearScale(val, minScaleFrom, maxScaleFrom, minScaleTo, maxScaleTo float64) float64 {
+	return (val-minScaleFrom)/(maxScaleFrom-minScaleFrom)*(maxScaleTo-minScaleTo) + minScaleTo
 }
--- src/buddhabrot/buddhabrot_test.go
+++ src/buddhabrot/buddhabrot_test.go
@@ -2,6 +2,8 @@
 package buddhabrot
 
 import (
+	"fmt"
+	"math"
 	"testing"
 
 	"github.com/ebeeton/buddhalbrot-go/parameters"
@@ -36,5 +38,52 @@ func TestPlot(t *testing.T) {
 		t.Errorf("Expected channel length %d, got %d",
 			int(plot.Width)*int(plot.Height), len(result[0]))
 	}
+}
 
+func TestLinearScale(t *testing.T) {
+	// "Close enough" from https://stackoverflow.com/a/47969546
+	const delta = 1e-10
+	var tests = []struct {
+		val, minScaleFrom, maxScaleFrom, minScaleTo, maxScaleTo, want float64
+	}{
+		{5, 0, 10, 0, 100, 50},
+		{50, 0, 100, 0, 1, 0.5},
+		{75, 0, 100, 0, 1, 0.75},
+		{0, -2, 0.47, 0, 3072, 2487.449392712551},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%f,%f,%f,%f,%f", tt.val, tt.minScaleFrom, tt.maxScaleFrom, tt.minScaleTo, tt.maxScaleTo)
+		t.Run(testname, func(t *testing.T) {
+			result := linearScale(tt.val, tt.minScaleFrom, tt.maxScaleFrom, tt.minScaleTo, tt.maxScaleTo)
+			if math.Abs(tt.want-result) > delta {
+				t.Errorf("Got %f, want %f", result, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsInMandelbrotSet(t *testing.T) {
+	var tests = []struct {
+		c             complex128
+		maxIterations int
+		isInSet       bool
+		iterations    int
+	}{
+		{complex(0, 0), 100, true, 100},
+		{complex(1, 1), 100, false, 1},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v,%d,%t,%d", tt.c, tt.maxIterations, tt.isInSet, tt.iterations)
+		t.Run(testname, func(t *testing.T) {
+			isInSet, iterations := isInMandelbrotSet(tt.c, tt.maxIterations)
+
+			if isInSet != tt.isInSet {
+				t.Errorf("Got %t, want %t", isInSet, tt.isInSet)
+			} else if iterations != tt.iterations {
+				t.Errorf("Got %d, want %d", iterations, tt.iterations)
+			}
+		})
+	}
 }
