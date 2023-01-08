99ac21ab647c986f6f2695422b89963cc7162445
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-01-08 15:21:21 -0500

    Add region to plot parameters.

--- src/parameters/rgbplot.go
+++ src/parameters/rgbplot.go
@@ -2,11 +2,13 @@
 package parameters
 
 // RgbPlot represents the parameters to plot an RGB Buddhabrot image of a given
-// width and height. Each color channel is configured independently.
+// width and height. Each color channel is configured independently. Region
+// defines the region on the complex plane to plot.
 type RgbPlot struct {
 	Red,
 	Green,
 	Blue Channel
+	Region Region
 	Width,
 	Height int
 }
