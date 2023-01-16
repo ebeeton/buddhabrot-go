7db62af8e624e2c43b44d3f85b40fb29e65e4753
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-01-16 17:56:53 -0500

    Add gradient package. Add stops to parameters.

--- parameters/plot.go
+++ parameters/plot.go
@@ -1,16 +1,20 @@
 // Package parameters defines parameters used to plot Buddhabrot images.
 package parameters
 
+import "github.com/ebeeton/buddhalbrot-go/gradient"
+
 // Plot represents the parameters to plot a Buddhabrot image of a given width
 // and height. SampleSize determines the number of sample points used.
 // MaxIterations is the maximum number of iterations to determine if a sample
 // point is in the Mandelbrot set. It is also the maximum number of iterations
 // to count orbits that pass through points visible in the plot. Region defines
-//the region on the complex plane to plot.
+// the region on the complex plane to plot. Gradient is a slice of colors and
+// their positions in a linear gradient used to color the image.
 type Plot struct {
 	SampleSize,
 	MaxIterations int
 	Region Region
 	Width,
 	Height int
+	Gradient []gradient.Stop
 }
