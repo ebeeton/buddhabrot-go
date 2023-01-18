2f5fa4aec175cede8d9ae2fee753698ebb523ecc
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-01-18 17:36:44 -0500

    Ensure 1.0 gradient stop is the last color.

--- gradient/gradient.go
+++ gradient/gradient.go
@@ -29,18 +29,23 @@ func GetGradient(stops []Stop, count int) []color.RGBA {
 		t = append(t, stop{col: c, pos: s.Position})
 	}
 
+	// Ensure that the values at either end of the gradient are the first and
+	// last colors.
+	step := 1.0 / float64(count-1)
 	for i := 0; i < count; i++ {
-		// TODO:: fix an off-by-one error here that prevents the stop at 1.0
-		// from being the last color in the resulting slice.
-		col := t.getInterpolatedColor(float64(i) / float64(count))
-		p = append(p, color.RGBA{
-			R: uint8(math.MaxUint8 * col.R),
-			G: uint8(math.MaxUint8 * col.G),
-			B: uint8(math.MaxUint8 * col.B),
-			A: math.MaxUint8,
-		})
+		col := t.getInterpolatedColor(float64(i) * step)
+		p = append(p, colorfulToColor(col))
 	}
 	return p
+}
+
+func colorfulToColor(c colorful.Color) color.RGBA {
+	return color.RGBA{
+		R: uint8(math.MaxUint8 * c.R),
+		G: uint8(math.MaxUint8 * c.G),
+		B: uint8(math.MaxUint8 * c.B),
+		A: math.MaxUint8,
+	}
 }
 
 func (gt gradientTable) getInterpolatedColor(t float64) colorful.Color {
@@ -50,7 +55,7 @@ func (gt gradientTable) getInterpolatedColor(t float64) colorful.Color {
 		if c1.pos <= t && t <= c2.pos {
 			// Blend the two points we're between.
 			t := (t - c1.pos) / (c2.pos - c1.pos)
-			return c1.col.BlendHcl(c2.col, t).Clamped()
+			return c1.col.BlendRgb(c2.col, t).Clamped()
 		}
 	}
 
--- gradient/gradient_test.go
+++ gradient/gradient_test.go
@@ -3,6 +3,8 @@
 package gradient
 
 import (
+	"image/color"
+	"math"
 	"testing"
 )
 
@@ -12,10 +14,20 @@ func TestGetGradient(t *testing.T) {
 		{Color: "#FFFFFF", Position: 1.0},
 	}
 
-	wantStops := 1000
+	wantStops := 3
 	g := GetGradient(stops, wantStops)
+
+	black := color.RGBA{R: 0, G: 0, B: 0, A: math.MaxUint8}
+	grey := color.RGBA{R: math.MaxUint8 >> 1, G: math.MaxUint8 >> 1, B: math.MaxUint8 >> 1, A: math.MaxUint8}
+	white := color.RGBA{R: math.MaxUint8, G: math.MaxUint8, B: math.MaxUint8, A: math.MaxUint8}
 
 	if len(g) != wantStops {
 		t.Errorf("Got stops %d, want %d.", len(g), wantStops)
+	} else if g[0] != black {
+		t.Errorf("Got black %v, want %v.", g[0], black)
+	} else if g[1] != grey {
+		t.Errorf("Got grey %v, want %v.", g[1], grey)
+	} else if g[2] != white {
+		t.Errorf("Got white %v, want %v.", g[2], white)
 	}
 }
